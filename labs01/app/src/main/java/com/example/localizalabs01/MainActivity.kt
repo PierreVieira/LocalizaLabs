14566beec21f14e72a133e1620eee3ffe6d56699
Pierre Vieira <49538805+PierreVieira@users.noreply.github.com>
2021-07-17 17:11:54 -0300

    [+] basic details screen

--- labs01/app/src/main/java/com/example/localizalabs01/MainActivity.kt
+++ labs01/app/src/main/java/com/example/localizalabs01/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.localizalabs01
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Configuration
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var changeThemeButton: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        changeThemeButton = findViewById(R.id.change_theme_icon)
+        setupChangeImageButton()
     }
+
+    private fun setupChangeImageButton() {
+        changeThemeButton.setOnClickListener {
+            when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+                Configuration.UI_MODE_NIGHT_YES -> setThemeColors(AppCompatDelegate.MODE_NIGHT_NO)
+                Configuration.UI_MODE_NIGHT_NO -> setThemeColors(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+        }
+    }
+
+    private fun setThemeColors(mode: Int) = AppCompatDelegate.setDefaultNightMode(mode)
 }
